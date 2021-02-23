d4b7134d3e28bd3d615b2f2f262b08d415842252
Ante Lovric <34556510+antelovric1423@users.noreply.github.com>
2021-02-23 23:59:38 +0100

    Implement ROS Node

---
+++ graphslam_pkg/scripts/run.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import rospy
+import world
+import slam
+from graphslam_pkg.srv import simulationStartService
+
+def handle_request_callback(req):
+    world_size = 100
+    measurement_range = 50.0
+    measurement_noise = 2.0
+    motion_noise = 2.0
+    
+    world = World(world_size, num_landmarks)
+    slam = SLAM2D(req.steps, world_size, req.num_landmarks, measurement_noise, motion_noise)
+    
+    # Make data and run SLAM
+    world.make_data(req.steps, measurement_range, motion_noise, measurement_noise)
+    slam.run_slam(world.data)
+    
+    # Get estimated robot and landmark positions
+    poses, landmarks = slam.get_poses_landmarks()
+    
+    # Print results
+    slam.show_results()
+    world.display_world(poses, landmarks)
+    
+    return simulationStartServiceResponse(true)
+    
+if __name__ == '__main__':
+    rospy.init_node('graphslam_server_node')
+    s = rospy.Service('graphslam_server', simulationStartService, handle_request_callback)
+    print("Init sequence done.")
+    rospy.spin()
